a6023ce38862077ef95baa9783eb83e35269d3bf
rahul sahay <rahulsahay19@hotmail.com>
2015-11-01 19:03:58 +0000

    2nd commit

---
+++ myRecordings/app/angularJS/angularJS.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'angularJS';
+    angular.module('app').controller(controllerId, ['common', angularJS]);
+
+    function angularJS(common) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.title = 'AngularJS';
+        vm.link = 'https://youtu.be/aHGmj_fqPLE';
+        activate();
+
+        function activate() {
+            common.activateController([], controllerId)
+                .then(function () { log('Activated angularJS View'); });
+        }
+    }
+})();
+
